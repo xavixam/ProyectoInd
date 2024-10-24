ec216089ffb3c149bd33d596ae1349d5ee7f358a
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-10-24 13:00:24 +0200

    Mostrar pregunta

---
+++ main.js
@@ -0,0 +1,37 @@
+const contenedorPreguntas = document.getElementById("contenedorPreguntas");
+const preguntas = document.getElementById("preguntas");
+const respuestas = document.getElementById("respuestas");
+const btnSiguiente = document.getElementById("siguiente");
+let api = "https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=boolean&encode=url3986"
+
+axios.get(api)
+
+.then((res) => {
+    const arrayPreguntas = res.data.results
+
+    for (const element of arrayPreguntas) {
+
+        preguntas.innerText = `${element.question}`
+
+    }
+})
+    
+.catch((err) => console.error(err))
+
+function mostrarPreguntas(pregunta) {
+
+    questionElement.innerText = question.question;
+
+    question.answers.forEach((respuesta) => {
+        
+      const btn = document.createElement("button");
+      btn.innerText = respuesta.text;
+  
+      if (respuesta.correct) {
+        btn.dataset.correct = true;
+      }
+  
+      answerButtonsElement.appendChild(btn);
+    });
+  }
+  

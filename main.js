be7f82f7f226536bf8da7008f955ed0d43f043dd
xavi <antunez.xavi.grao@gmail.com>
2024-10-25 12:53:36 +0200

    He conseguido que muestre todas las preguntas

--- main.js
+++ main.js
@@ -3,29 +3,35 @@ const preguntas = document.getElementById("preguntas");
 const respuestas = document.getElementById("respuestas");
 const btnSiguiente = document.getElementById("siguiente");
 let api = "https://opentdb.com/api.php?amount=5&category=15&difficulty=medium&type=multiple&encode=url3986"
+let contador = 0;  
 
 axios.get(api)
 
 .then((res) => {
     const arrayPreguntas = res.data.results  
-    let contador = 0;  
 
-    preguntas.innerText = `${arrayPreguntas[contador].question}`
-    respuestas.innerHTML = `<button id="correcta">${arrayPreguntas[contador].correct_answer}</button> <button id="incorrecta">${arrayPreguntas[contador].incorrect_answers}</button>`        
+    mostrarPreguntas(arrayPreguntas, contador)  
 
+    function mostrarPreguntas(arrayPreguntas, contador) {
+        preguntas.innerText = `${arrayPreguntas[contador].question}`
+        respuestas.innerHTML = `<button id="correcta">${arrayPreguntas[contador].correct_answer}</button>
+        <button id="incorrecta1">${arrayPreguntas[contador].incorrect_answers[0]}</button>
+        <button id="incorrecta2">${arrayPreguntas[contador].incorrect_answers[1]}</button>
+        <button id="incorrecta3">${arrayPreguntas[contador].incorrect_answers[2]}</button>`
+    }
+    
+    function limpiar() {
+        preguntas.innerText = ``
+        respuestas.innerHTML = ``
+    }
+    
+    function siguientePregunta() {
+        limpiar()
+        contador = contador + 1
+        mostrarPreguntas(arrayPreguntas, contador)
+    }
+
+    btnSiguiente.addEventListener("click", siguientePregunta)
 })
     
 .catch((err) => console.error(err))
-
-btnSiguiente.addEventListener("click", siguientePregunta)
-
-function limpiar() {
-    
-    preguntas.innerText = ``
-    respuestas.innerHTML = ``        
-    contador + 1
-}
-
-function siguientePregunta() {
-    limpiar()
-}
